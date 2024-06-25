bc0120faaf49aab37c4f4973b32189505968b1dd
NguyenMinhNhat82 <20510520925nhat@ou.edu.vn>
2024-06-25 23:28:48 +0700

    aa

--- src/main/java/com/controller/iot/IOTControllerApplication.java
+++ src/main/java/com/controller/iot/IOTControllerApplication.java
@@ -5,8 +5,10 @@ import org.eclipse.paho.client.mqttv3.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.http.ResponseEntity;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.web.client.RestTemplate;
 
 import java.io.IOException;
 import java.security.GeneralSecurityException;
@@ -25,5 +27,18 @@ public class IOTControllerApplication {
     public synchronized void autoGenerateNumber() throws GeneralSecurityException, IOException {
         System.out.println("Pub data");
         iotService.publicCurrentState();
+    }
+
+    @Scheduled(fixedRate = 1000*60*2)
+    public synchronized void keepServerAlive() throws GeneralSecurityException, IOException {
+        RestTemplate restTemplate = new RestTemplate();
+        System.out.println("reset");
+        String url =  "https://iotcontroller-1.onrender.com/test";
+        //http://localhost:9000
+//        https://serveriot-ob37.onrender.com/
+
+        //https://serveriot-1.onrender.com/test
+        ResponseEntity<String> response
+                = restTemplate.getForEntity(url , String.class);
     }
 }
